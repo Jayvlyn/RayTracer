4d22ae34937e7ca0916fedfcd1f9d7b20a19ce4f
Jacob Leonardo <leonardojacob257@gmail.com>
2023-12-04 13:51:51 -0700

    Metal and Blue balls

--- Source/RayTracer/Main.cpp
+++ Source/RayTracer/Main.cpp
@@ -4,11 +4,14 @@
 #include "Canvas.h"
 #include "Camera.h"
 #include "Scene.h"
+#include "Material.h"
+#include "Sphere.h"
+#include <vector>
 
 int main(int, char**)
 {
-	static int width = 400;
-	static int height = 300;
+	static int width = 800;
+	static int height = 600;
 
 	//std::cout << "Hello world\n";
 	seedRandom((unsigned int)time(nullptr));
@@ -21,12 +24,31 @@ int main(int, char**)
 	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
 	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 0, 1 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 70.0f, aspectRatio);
 
-	Scene scene; // sky color could be set with the top and bottom color
+	Scene scene(10);
 	scene.SetCamera(camera);
+
+	// create material
+	auto lambertian = std::make_shared<Lambertian>(color3_t{ 0, 0, 1 });
+	auto metal = std::make_shared<Metal>(color3_t{ 1, 1, 1 }, 0.0f);
+
+	// create objects -> add to scene
+	for (int i = 0; i < 10; i++)
+	{
+		std::shared_ptr<Material> material = ((int)random(0,2) == 0) ? std::dynamic_pointer_cast<Material>(lambertian) : std::dynamic_pointer_cast<Material>(metal);
+		auto sphere = std::make_unique<Sphere>(glm::vec3{ random(-2,2), random(-2,2), random(-3,0) }, random(0.1, 0.5), material);
+		scene.AddObject(std::move(sphere));
+	}
+
+	// render scene 
+	canvas.Clear({ 0, 0, 0, 1 });
+	scene.Render(canvas, 10);
+	canvas.Update();
 
 	bool quit = false;
 	while (!quit)
 	{
+		renderer.PresentCanvas(canvas);
+
 		SDL_Event event;
 		SDL_PollEvent(&event);
 		switch (event.type)
@@ -43,18 +65,6 @@ int main(int, char**)
 			}
 			break;
 		}
-
-		/*canvas.Clear({ 0, 0, 0, 1 });
-		for (int i = 0; i < 1000; i++) canvas.DrawPoint({ random(0, width), random(0,height)}, {random01(), random01(), random01(), 1});
-		canvas.Update();
-
-		renderer.PresentCanvas(canvas);*/
-
-		canvas.Clear({ 0, 0, 0, 1 });
-		scene.Render(canvas);
-		canvas.Update();
-
-		renderer.PresentCanvas(canvas);
 	}
 
 	renderer.Shutdown();
